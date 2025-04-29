c11279349fb71e79302c993be5dea04707d9ed4c
aeg6430 <aeg6430@gmail.com>
2025-04-29 20:51:19 +0800

    refactor(home): make HomeScreen stateful to access theme brightness
    - Converted HomeScreen from StatelessWidget to StatefulWidget to properly use Theme.of(context).brightness
    - Updated tab configuration accordingly
    - Add i18n support placeholder for tab labels (Map, Favorites, Settings)

--- apps/mobile/lib/view/screens/home_screen/home_screen.dart
+++ apps/mobile/lib/view/screens/home_screen/home_screen.dart
@@ -1,21 +1,32 @@
+import 'package:caffeing/l10n/generated/l10n.dart';
 import 'package:caffeing/view/screens/favorite/store/favorite_store_screen.dart';
 import 'package:caffeing/view/screens/map_screen/map_screen.dart';
 import 'package:caffeing/view/screens/settings_screen/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final PersistentTabController _controller = PersistentTabController(
     initialIndex: 0,
   );
-  List<PersistentTabConfig> _tabs() => [
+
+  List<PersistentTabConfig> _tabs(BuildContext context) => [
     PersistentTabConfig(
       screen: MapScreen(),
       item: ItemConfig(
         icon: const Icon(Icons.map),
-        title: "Map",
-        activeForegroundColor: Colors.blue,
+        title: S.of(context).map,
+        activeForegroundColor:
+            Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Colors.black,
         inactiveForegroundColor: Colors.grey,
       ),
     ),
@@ -23,8 +34,11 @@ class HomeScreen extends StatelessWidget {
       screen: FavoriteStoreScreen(controller: _controller),
       item: ItemConfig(
         icon: const Icon(Icons.favorite),
-        title: "Favorites",
-        activeForegroundColor: Colors.red,
+        title: S.of(context).favorites,
+        activeForegroundColor:
+            Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Colors.black,
         inactiveForegroundColor: Colors.grey,
       ),
     ),
@@ -32,23 +46,27 @@ class HomeScreen extends StatelessWidget {
       screen: SettingsScreen(),
       item: ItemConfig(
         icon: const Icon(Icons.settings),
-        title: "Settings",
-        activeForegroundColor: Colors.green,
+        title: S.of(context).settings,
+        activeForegroundColor:
+            Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Colors.black,
         inactiveForegroundColor: Colors.grey,
       ),
     ),
   ];
 
   @override
-  Widget build(BuildContext context) => PersistentTabView(
-    tabs: _tabs(),
-    controller: _controller,
-    navBarBuilder:
-        (navBarConfig) => Style1BottomNavBar(
-          navBarConfig: navBarConfig,
-          navBarDecoration: NavBarDecoration(color: Colors.transparent),
-        ),
-    navBarHeight: 60,
-    navBarOverlap: NavBarOverlap.full(),
-  );
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      tabs: _tabs(context),
+      controller: _controller,
+      navBarBuilder:
+          (navBarConfig) => Style1BottomNavBar(
+            navBarConfig: navBarConfig,
+            navBarDecoration: NavBarDecoration(color: Colors.transparent),
+          ),
+      navBarOverlap: NavBarOverlap.full(),
+    );
+  }
 }
