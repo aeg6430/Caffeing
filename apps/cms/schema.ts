65e62f599b7be696e59ea1f3cd014db5eeff4a2d
aeg6430 <aeg6430@gmail.com>
2025-05-11 13:29:17 +0800

    refactor: rename files to kebab-case for TypeScript consistency

--- apps/cms/schema.ts
+++ apps/cms/schema.ts
@@ -1,9 +1,9 @@
 import { type Lists } from ".keystone/types";
 import { User } from "./schema/user";
-import { AppUser } from "./schema/app_user";
-import { FavoriteStore } from "./schema/favorite_store";
+import { AppUser } from "./schema/app-user";
+import { FavoriteStore } from "./schema/favorite-store";
 import { Keyword } from "./schema/keyword";
-import { StoreKeyword } from "./schema/store_keyword";
+import { StoreKeyword } from "./schema/store-keyword";
 import { Store } from "./schema/store";
 import { SuggestedStore } from './schema/suggested-store';
 
